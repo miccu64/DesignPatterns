07d3f8cd00e07759a9742e983b5350e77dab2210
miccu64 <98konrad98@gmail.com>
2025-07-20 18:37:40 +0200

    Author repair

---
+++ DesignPatterns.Tests/Tests/Prototype/LinePrototypeTests.cs
@@ -0,0 +1,35 @@
+﻿using DesignPatterns.Prototype;
+using DesignPatterns.Prototype.Models;
+using Xunit;
+
+namespace DesignPatterns.Tests.Tests.Prototype
+{
+    public class LinePrototypeTests
+    {
+        [Fact]
+        public void Should_Create_Deep_Copy()
+        {
+            // Arrange
+            Point point = new()
+            {
+                X = 1,
+                Y = 2
+            };
+            LinePrototype line = new()
+            {
+                Start = point,
+                End = point,
+                Name = "SomeName"
+            };
+
+            // Act
+            LinePrototype copy = line.DeepCopy();
+
+            // Assert
+            Assert.NotSame(line, copy);
+            Assert.NotSame(line.Start, copy.Start);
+            Assert.NotSame(line.End, copy.End);
+            Assert.Equal(line.Name, copy.Name);
+        }
+    }
+}
---
+++ DesignPatterns/Prototype/LinePrototype.cs
@@ -0,0 +1,21 @@
+﻿using DesignPatterns.Prototype.Models;
+
+namespace DesignPatterns.Prototype
+{
+    public class LinePrototype
+    {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+        public string Name { get; set; }
+
+        public LinePrototype DeepCopy()
+        {
+            LinePrototype copy = (LinePrototype)MemberwiseClone();
+
+            copy.Start = Start.DeepCopy();
+            copy.End = End.DeepCopy();
+
+            return copy;
+        }
+    }
+}
