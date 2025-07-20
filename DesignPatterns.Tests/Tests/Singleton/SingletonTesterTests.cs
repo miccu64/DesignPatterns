a03b57523196a4dd01a2bb80f49a75df8e65b437
Konrad Micek <konrad.micek@proton.me>
2025-07-20 21:37:29 +0200

    Singleton

---
+++ DesignPatterns.Tests/Tests/Singleton/SingletonTesterTests.cs
@@ -0,0 +1,43 @@
+﻿using DesignPatterns.Singleton;
+using Xunit;
+
+namespace DesignPatterns.Tests.Tests.Singleton
+{
+    public class SingletonTesterTests
+    {
+        [Fact]
+        public void Should_Return_True_On_Singleton()
+        {
+            // Arrange
+            Func<object> singletonFactory = () => TestSingleton.Instance;
+
+            // Act
+            bool isSingleton = SingletonTester.IsSingleton(singletonFactory);
+        
+            // Assert
+            Assert.True(isSingleton);
+        }
+
+        [Fact]
+        public void Should_Return_False_On_Same_Instance_Of_Regular_Class()
+        {
+            // Arrange
+            Func<object> factory = () => new SingletonTesterTests();
+
+            // Act
+            bool isSingleton = SingletonTester.IsSingleton(factory);
+
+            // Assert
+            Assert.False(isSingleton);
+        }
+
+        private sealed class TestSingleton
+        {
+            private static readonly Lazy<TestSingleton> _instance = new(() => new TestSingleton());
+
+            public static TestSingleton Instance => _instance.Value;
+
+            private TestSingleton() { }
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/SingletonTester.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesignPatterns.Singleton
+{
+    public static class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            object obj1 = func();
+            object obj2 = func();
+
+            if (obj1 != obj2)
+                return false;
+
+            return obj1 == obj2;
+        }
+    }
+}
