1b928e1a7bffc7a1fbcf15f7062ce582dafcb009
Konrad Micek <konrad.micek@proton.me>
2025-07-20 18:37:40 +0200

    Prototype

---
+++ DesignPatterns.Tests/Tests/Prototype/PointTests.cs
@@ -0,0 +1,27 @@
+﻿using DesignPatterns.Prototype.Models;
+using Xunit;
+
+namespace DesignPatterns.Tests.Tests.Prototype
+{
+    public class PointTests
+    {
+        [Fact]
+        public void Should_Create_Deep_Copy()
+        {
+            // Arrange
+            Point point = new()
+            {
+                X = 1,
+                Y = 2
+            };
+
+            // Act
+            Point copy = point.DeepCopy();
+
+            // Assert
+            Assert.NotSame(point, copy);
+            Assert.Equal(point.X, copy.X);
+            Assert.Equal(point.Y, copy.Y);
+        }
+    }
+}
---
+++ DesignPatterns/Prototype/Models/Point.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Prototype.Models
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point DeepCopy()
+        {
+            return (Point)MemberwiseClone();
+        }
+    }
+}
