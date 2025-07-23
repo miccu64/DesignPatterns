a66ba9d2c2371eeddc0105384e2882def1b5b560
Konrad Micek <konrad.micek@proton.me>
2025-07-23 21:55:49 +0200

    Adapter

---
+++ DesignPatterns/Adapter/SquareToRectangleAdapter.cs
@@ -0,0 +1,18 @@
+﻿using DesignPatterns.Adapter.Interfaces;
+using DesignPatterns.Adapter.Models;
+
+namespace DesignPatterns.Adapter
+{
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        public int Width => _square.Side;
+        public int Height => _square.Side;
+
+        private readonly Square _square;
+
+        public SquareToRectangleAdapter(Square square)
+        {
+            _square = square;
+        }
+    }
+}
