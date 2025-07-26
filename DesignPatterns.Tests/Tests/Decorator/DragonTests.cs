498c0a22e144e859c64a3f76a8cf2f2ff166d424
miccu64 <98konrad98@gmail.com>
2025-07-26 17:05:19 +0200

    Decorator

---
+++ DesignPatterns.Tests/Tests/Decorator/DragonTests.cs
@@ -0,0 +1,25 @@
+﻿using DesignPatterns.Decorator;
+using Xunit;
+
+namespace DesignPatterns.Tests.Tests.Decorator
+{
+    public class DragonTests
+    {
+        [Fact]
+        public void Should_Properly_Create()
+        {
+            // Arrange
+            const int age = 10;
+            Dragon dragon = new() { Age = age };
+
+            // Act
+            string fly = dragon.Fly();
+            string crawl = dragon.Crawl();
+
+            // Assert
+            Assert.NotEmpty(fly);
+            Assert.NotEmpty(crawl);
+            Assert.Equal(age, dragon.Age);
+        }
+    }
+}
