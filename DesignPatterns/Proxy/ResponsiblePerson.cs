eddc91dea688cdc5d528b74149e8aff2a1da4133
Konrad Micek <konrad.micek@proton.me>
2025-07-29 20:29:07 +0200

    Proxy

---
+++ DesignPatterns/Proxy/ResponsiblePerson.cs
@@ -0,0 +1,39 @@
+﻿using DesignPatterns.Proxy.Models;
+
+namespace DesignPatterns.Proxy
+{
+    public class ResponsiblePerson
+    {
+        private readonly Person _person;
+
+        public int Age
+        {
+            get => _person.Age;
+            set => _person.Age = value;
+        }
+
+        public ResponsiblePerson(Person person)
+        {
+            _person = person;
+        }
+
+        public string Drink()
+        {
+            return _person.Age < 18
+                ? "too young"
+                : _person.Drink();
+        }
+
+        public string Drive()
+        {
+            return _person.Age < 16
+                ? "too young"
+                : _person.Drive();
+        }
+
+        public string DrinkAndDrive()
+        {
+            return "dead";
+        }
+    }
+}
