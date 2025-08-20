47a1ddad94283ad0b59c585125a38883ac31f1bf
miccu64 <98konrad98@gmail.com>
2025-08-20 22:13:44 +0200

    Iterator

---
+++ DesignPatterns.Tests/Tests/Iterator/NodeTests.cs
@@ -0,0 +1,38 @@
+﻿using DesignPatterns.Iterator;
+using Xunit;
+
+namespace DesignPatterns.Tests.Tests.Iterator
+{
+    public class NodeTests
+    {
+        [Fact]
+        public void Should_Iterate_Binary_Tree_In_Preorder()
+        {
+            // Arrange
+            Node<string> c = new("c");
+            Node<string> e = new("e");
+
+            Node<string> d = new("d", c, e);
+            Node<string> a = new("a");
+
+            Node<string> b = new("b", a, d);
+
+            Node<string> h = new("h");
+
+            Node<string> i = new("i", h, null);
+
+            Node<string> g = new("g", null, i);
+
+            Node<string> f = new("f", b, g);
+
+            // Act
+            string result = "";
+            foreach (string value in f.PreOrder)
+                result += value;
+
+            // Assert
+            const string expectedResult = "fbadcegih";
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ DesignPatterns/Iterator/Node.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Iterator
+{
+    public class Node<T>
+    {
+        public T Value { get; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+        public Node<T> Parent { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right)
+            : this(value)
+        {
+            Left = left;
+            Right = right;
+
+            if (left != null)
+                left.Parent = this;
+            if (right != null)
+                right.Parent = this;
+        }
+
+        public IEnumerable<T> PreOrder
+        {
+            get
+            {
+                yield return Value;
+
+                if (Left != null)
+                {
+                    foreach (T value in Left?.PreOrder)
+                        yield return value;
+                }
+                if (Right != null)
+                {
+                    foreach (T value in Right?.PreOrder)
+                        yield return value;
+                }
+            }
+        }
+    }
+}
