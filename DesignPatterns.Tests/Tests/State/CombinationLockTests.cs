9503dcadf8ea0ce3df6a881b07d15d159d262764
miccu64 <98konrad98@gmail.com>
2025-10-19 16:32:34 +0200

    State

---
+++ DesignPatterns.Tests/Tests/State/CombinationLockTests.cs
@@ -0,0 +1,59 @@
+﻿using DesignPatterns.State;
+using Xunit;
+
+namespace DesignPatterns.Tests.Tests.State
+{
+    public class CombinationLockTests
+    {
+
+        [Fact]
+        public void Should_Be_Locked_After_Init()
+        {
+            // Arrange / Act
+            CombinationLock combinationLock = new([1]);
+
+            // Assert
+            Assert.Equal(CombinationLock.StatusLocked, combinationLock.Status);
+        }
+
+        [Fact]
+        public void Should_Open_Lock_Returning_Correct_Value_After_Each_Digit()
+        {
+            // Arrange
+            List<int> values = [1, 2, 3, 4, 5];
+            CombinationLock combinationLock = new(values);
+
+            // Act / Assert
+            string joinedValues = "";
+            for (int i = 0; i < values.Count; i++)
+            {
+                int value = values[i];
+                combinationLock.EnterDigit(value);
+
+                joinedValues += value;
+
+                if (i < values.Count - 1)
+                    Assert.Equal(joinedValues, combinationLock.Status);
+            }
+
+            Assert.Equal(CombinationLock.StatusOpen, combinationLock.Status);
+        }
+
+        [Fact]
+        public void Should_Show_Error_When_Wrong_Code()
+        {
+            // Arrange
+            List<int> values = [1, 2, 3];
+            CombinationLock combinationLock = new(values);
+
+            List<int> wrongValues = [1, 2, 4];
+
+            // Act
+            foreach (int value in wrongValues)
+                combinationLock.EnterDigit(value);
+
+            // Assert
+            Assert.Equal(CombinationLock.StatusError, combinationLock.Status);
+        }
+    }
+}
---
+++ DesignPatterns/State/CombinationLock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignPatterns.State
+{
+    public class CombinationLock
+    {
+        public static string StatusLocked = "LOCKED";
+        public static string StatusOpen = "OPEN";
+        public static string StatusError = "ERROR";
+
+        public string Status { get; private set; }
+
+        private readonly string _combination;
+
+        public CombinationLock(IEnumerable<int> combination)
+        {
+            if (combination?.Any() != true)
+                throw new ArgumentException("Incorrect combination.");
+
+            _combination = string.Join("", combination);
+
+            Status = StatusLocked;
+        }
+
+        public void EnterDigit(int digit)
+        {
+            if (digit < 0 || digit > 9)
+                throw new ArgumentOutOfRangeException(nameof(digit));
+
+            bool isCombinationInput = char.IsDigit(Status[0]);
+            if (isCombinationInput)
+            {
+                Status += digit;
+
+                bool lengthAchieved = Status.Length == _combination.Length;
+                if (lengthAchieved)
+                {
+                    Status = Status == _combination
+                        ? StatusOpen
+                        : StatusError;
+                }
+            }
+            else
+            {
+                Status = digit.ToString();
+            }
+        }
+    }
+}
